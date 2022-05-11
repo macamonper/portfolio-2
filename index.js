5f75b5c06d47fdc9e7b10bc29431323069e25e22
macarena <macamonper@gmail.com>
2022-05-11 01:46:01 -0300

    js added to html to figure out why is not working

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector(".mobile-menu");
 const burgerMenu = document.querySelector(".burger-menu");
 const aside = document.querySelector("aside");
-const form = document.querySelector("form");
 const email= document.getElementById("email").value
 
 let menuOpen = false;
@@ -19,23 +18,5 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
-  <script src="https://smtpjs.com/v3/smtp.js"></script>;
 
-const sendEmail = () => {
-Email.send({
-  SecureToken: "1fe58c20-8c18-4962-a8c3-82bbf33d9028",
-  To: "macamonper@gmail.com",
-  From: email,
-  Subject: "New contact from portfolio",
-  Body: "And this is the body",
-  Attachments: [
-    {
-      name: "smtpjs.png",
-      path: "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png",
-    },
-  ],
-}).then((message) => alert(message));
-
-
-};
 
