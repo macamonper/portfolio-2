1518108f1cabdbfe352e7c83ddbdbaff0ce868ea
macarena <macamonper@gmail.com>
2022-05-12 15:00:31 -0300

    hamburguer menu fixed

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const menuBtn = document.querySelector(".mobile-menu");
-const burgerMenu = document.querySelector(".burger-menu");
-const aside = document.querySelector("aside");
-
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-    burgerMenu.classList.remove("hidden");
-    aside.style.backgroundColor = "#0c0d17d2";
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-    burgerMenu.classList.add("hidden");
-    aside.style.backgroundColor = "transparent";
-  }
-});
-
-
-
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
